9429977664432a0179919885f48ea881390309f1
François Jean Dazin <francois.jean.dazin@gmail.com>
2025-09-17 16:17:24 +0200

    Fix: remove node-fetch import

--- netlify/functions/bergson.js
+++ netlify/functions/bergson.js
@@ -1,5 +1,4 @@
 // netlify/functions/bergson.js
-const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
     // CORS headers pour permettre les appels depuis le frontend
